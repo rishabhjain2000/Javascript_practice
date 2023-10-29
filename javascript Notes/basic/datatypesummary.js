b31638c12363e364f294210d319926d68be53f06
rishabhjain2000 <rishabhjainnitbhopal@gmail.com>
2023-10-29 12:27:27 +0530

    data type summary

---
+++ javascript Notes/basic/datatypesummary.js
@@ -0,0 +1,66 @@
+/*
+JavaScript is a dynamically typed language. 
+This means that variable types are determined at runtime,
+ and you do not need to explicitly declare the type of a 
+ variable before using it. You can assign different types of
+  values to a variable during its lifetime.
+
+For example, in JavaScript, you can do the following:
+
+let x = 10; // x is now a number
+x = "Hello"; // x is now a string
+x = true; // x is now a boolean
+On the other hand, statically typed languages require you to declare the variable's type explicitly, and the type checking is done at compile-time, before the code is executed
+ */
+
+
+/* 
+typeof "John"                 // Returns "string"
+typeof 3.14                   // Returns "number"
+typeof NaN                    // Returns "number"
+typeof false                  // Returns "boolean"
+typeof [1,2,3,4]              // Returns "object"
+typeof {name:'John', age:34}  // Returns "object"
+typeof new Date()             // Returns "object"
+typeof function () {}         // Returns "function"
+typeof myCar                  // Returns "undefined" *
+typeof null                   // Returns "object"
+*/
+
+//  Primitive
+
+//  7 types : String, Number, Boolean, null, undefined,
+//            Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId); //false
+
+// const bigNumber = 3456543576654356754n // put n in last 
+
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof anotherId);
+console.log(typeof myFunction);
